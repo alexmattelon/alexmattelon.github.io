1e7bb5fcb28860754f2108d98266f27f760b0b46
Alexandre Mattelon <alexandre.mattelon@gmail.com>
2016-11-19 01:20:03 +0100

    Portfolio update
    Added new portfolio items

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 $(document).ready( function() {
 	// Logo
 	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
-    }
-    
-	// Show logo 
+	if (location.href.indexOf("#") != -1) {
+		if(location.href.substr(location.href.indexOf("#"))!='#about'){
+			$logo.show();
+		}
+	}
+
+	// Show logo
 	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
+		$logo.slideDown('slow');
 	});
 	// Hide logo
 	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
-    
-    function animMeter(){
-        $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-    }
-    animMeter();
+		$logo.slideUp('slow');
+	});
+
+	function animMeter(){
+		$(".meter > span").each(function() {
+			$(this)
+			.data("origWidth", $(this).width())
+			.width(0)
+			.animate({
+				width: $(this).data("origWidth")
+			}, 1200);
+		});
+	}
+	animMeter();
 
 	$('#tab-container').easytabs({
 		animate			: true,
@@ -36,18 +36,18 @@ $(document).ready( function() {
 		animationSpeed	: 600,
 		tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
 			if($targetPanel.selector=='#resume'){
-					animMeter();
+				animMeter();
 			}
-	});
-	  
+		});
+
 	// Language selection
-	$(".FR").hide(); 
+	$(".FR").hide();
 	$('.button-fr').click(function(){
-		$(".EN").hide(); 
-		$(".FR").show(); 
+		$(".EN").hide();
+		$(".FR").show();
 	});
 	$('.button-en').click(function(){
-		$(".FR").hide(); 
-		$(".EN").show(); 
+		$(".FR").hide();
+		$(".EN").show();
 	});
 });
